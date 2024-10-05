d87b4ebbb7ab127ef41dd45ce12a0fcab8e35f96
raji-ayyub <ayyubraji87@gmail.com>
2024-10-05 15:11:49 +0100

    fix:image load issues

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      domains: ['your-domain.com', 'another-domain.com'], // Replace with your actual image domains
+    },
+  };
+  
+  export default nextConfig;
+  
